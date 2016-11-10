e7641100c3c3ef40cf63b2c79d22f84f0e10da02
山本元柳斎権三郎 <panis@outlook.jp>
2016-11-10 18:35:13 +0900

    実装 ListViewとButtonのビューの調整をした

--- app/src/main/java/com/example/owner/warewolf/ClassListActivity.java
+++ app/src/main/java/com/example/owner/warewolf/ClassListActivity.java
@@ -1,9 +1,15 @@
 package com.example.owner.warewolf;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ListView;
 
-public class ClassListActivity extends AppCompatActivity {
+public class ClassListActivity extends Activity {
+
+    Button bt_next_button3;
+    ListView lv_class_list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -11,6 +17,9 @@ public class ClassListActivity extends AppCompatActivity {
         setContentView(R.layout.activity_class_list);
 
         getIntent();
+
+        bt_next_button3 = (Button)findViewById(R.id.NextButton3);
+        lv_class_list= (ListView)findViewById(R.id.ClassList);
 
     }
 }

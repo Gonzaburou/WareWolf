cf20cc05eb8cd5499d50617a023afa76ca57e03a
山本元柳斎権三郎 <panis@outlook.jp>
2016-11-11 18:50:54 +0900

    [実装] プリファレンスファイルの追加

--- app/src/main/java/com/example/owner/warewolf/MemberSettingActivity.java
+++ app/src/main/java/com/example/owner/warewolf/MemberSettingActivity.java
@@ -2,6 +2,7 @@ package com.example.owner.warewolf;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.SpannableStringBuilder;
 import android.view.View;
@@ -9,6 +10,14 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+
+import static android.R.attr.key;
+import static android.R.attr.value;
 
 public class MemberSettingActivity extends Activity {
 
@@ -41,9 +50,14 @@ public class MemberSettingActivity extends Activity {
             public void onClick(View view) {
                 sb_input_name = (SpannableStringBuilder)et_input_name.getText();
                 et_input_name.setText("");
-                if(sb_input_name.toString().isEmpty() == false) {
+                if(sb_input_name.toString().isEmpty() == false){
                     aa_input_name.add(sb_input_name.toString());
                     lv_member_list.setAdapter(aa_input_name);
+
+                    SharedPreferences pref = getSharedPreferences("pref",MODE_PRIVATE);
+                    SharedPreferences.Editor e = pref.edit();
+                    e.putString("key", sb_input_name.toString());
+                    e.commit();
                 }
             }
         });
